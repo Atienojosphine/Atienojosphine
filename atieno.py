52f4d0e6af4d491088ac98936337f96b373b4072
Atienojosphine <joyatieno201@gmail.com>
2024-11-04 04:23:12 -0800

    Add files via upload

---
+++ atieno.py
@@ -0,0 +1,17 @@
+# This a python string
+
+x="Josephine"
+print(x)
+
+
+# this is how we get the data type of variable value(casting)
+y="John"
+print(type(y))
+
+z=5
+print(type(z))
+
+# This is an integer
+y=5
+print(y)
+
